a3ffd1d7020356a9437d95098dd516b862f0f48a
Riccardo Spadon <spadon.riccardo@gmail.com>
2026-03-05 19:06:41 +0100

    Create server.js
    Create server for check the status

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API Immobiliare Morini funzionante 🚀' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server avviato sulla porta ${PORT}`);
+});
