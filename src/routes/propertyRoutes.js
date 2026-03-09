0758f36e3835a44d7363672fcdec32f007c5bd80
Riccardo Spadon <spadon.riccardo@gmail.com>
2026-03-09 21:29:21 +0100

    Update propertyRoutes.js
    JWT middleware working and routes protected

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { getProperties, createProperty, getPropertyById, updateProperty, deleteProperty } from '../controllers/propertyControllers.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+
 router.get('/', getProperties);
-router.post('/', createProperty);
+router.post('/', authMiddleware, createProperty);
 router.get('/:id', getPropertyById);
-router.put('/:id', updateProperty);
-router.delete('/:id', deleteProperty);
+router.put('/:id', authMiddleware, updateProperty);
+router.delete('/:id', authMiddleware, deleteProperty);
 
 export default router;
