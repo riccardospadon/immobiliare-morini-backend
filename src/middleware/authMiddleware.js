8eca87b314bad91479137d3006a991eff9745186
Riccardo Spadon <spadon.riccardo@gmail.com>
2026-03-09 20:32:08 +0100

    Create authMiddleware
    Created authMiddleware.js for verify if the admin is connected

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json({ message: "Token mancante" })
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded
+        next();
+    } catch(error){
+        return res.status(401).json({ message: "Token non valido" })
+    }
+}
+
+export default authMiddleware;
