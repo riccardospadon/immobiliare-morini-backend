ab54db2303df00b4a1204b762b1470f047287f24
Riccardo Spadon <spadon.riccardo@gmail.com>
2026-03-10 19:07:05 +0100

    Update propertyControllers.js
    Add filters, sorting and pagination to properties endpoint

--- src/controllers/propertyControllers.js
+++ src/controllers/propertyControllers.js
@@ -1,10 +1,11 @@
 import Property from '../models/property.js';
 
-// GET all properties
+// GET all properties with filters, sorting and pagination
 export const getProperties = async (req, res) => {
     try{
 
-        const { city, type, category } = req.query;
+        const { city, type, category, sort, page=1, limit=6 } = req.query;
+
         const filters ={}
         if(city){
             filters.city = city;
@@ -16,8 +17,29 @@ export const getProperties = async (req, res) => {
             filters.category = category;
         }
 
-        const properties = await Property.find(filters);
-        res.json(properties);
+        let sortOption = {};
+        if(sort === 'price_asc'){
+            sortOption.price = 1;
+        } else if(sort === 'price_desc'){
+            sortOption.price = -1;
+        } else if(sort === 'newest'){
+            sortOption.createdAt = -1; 
+        } else if (sort === 'oldest'){
+            sortOption.createdAt = 1;
+        }
+
+        const currentPage = parseInt(page);
+        const perPage = parseInt(limit);
+        const skip = (currentPage - 1) * perPage;
+        const total = await Property.countDocuments(filters);
+
+        const properties = await Property.find(filters).sort(sortOption).skip(skip).limit(perPage);
+        res.json({
+            properties,
+            currentPage,
+            totalPages: Math.ceil(total / perPage),
+            properties
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
